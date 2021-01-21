e696d8afaa351ff7a4ef2e9fd34e5ae31c310f4c
Ahmad El Gamal <ahmadelgamal@gmail.com>
2021-01-20 16:23:21 -0800

    Updates relative path to graphql in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3001/graphql'
+  uri: '/graphql'
 });
 
 function App() {
