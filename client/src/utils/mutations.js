01834afd6741090fbc3153d29f0c5869676be75c
Ahmad El Gamal <ahmadelgamal@gmail.com>
2021-01-20 20:05:09 -0800

    Cleans up mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -44,10 +44,17 @@ export const SAVE_BOOK = gql`
 export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: ID!) {
     removeBook(bookId: $bookId) {
-      user {
         _id
-        username
-        email
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
       }
     }
   }
